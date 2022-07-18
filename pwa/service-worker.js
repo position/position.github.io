e4468a44d06038354061149644047f03e7afc889
Benjamin Choi <benjamin.choi@nsuslab.com>
2022-07-18 17:49:40 +0900

    PWA 테스트

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,126 +1,128 @@
 const CACHE_NAME = "pwatest-v7";
 const OFFLINE_URL = "./offline.html";
 const ASSETS = [
-  "./roboto-black.e79330321758be9bc5db.woff",
-  "./icon-192x192.png",
-  "./roboto-light.7edb5c1283fd64425d81.woff",
+	"./roboto-black.e79330321758be9bc5db.woff",
+	"./icon-192x192.png",
+	"./roboto-light.7edb5c1283fd64425d81.woff",
+	"/offline.html"
 ];
 const channel = new BroadcastChannel("sw-messages");
 
 //install
 self.addEventListener("install", (e) => {
-  channel.postMessage({ title: "postmessage test" });
-  e.waitUntil(
-    (async () => {
-      try {
-        console.log("서비스워커 Install");
+	channel.postMessage({ title: "postmessage test" });
+	e.waitUntil(
+		(async () => {
+			try {
+				console.log("서비스워커 Install");
 
-        const cache = await caches.open(CACHE_NAME);
-        await cache.addAll([
-          new Request(OFFLINE_URL, { cache: "reload" }),
-          ...ASSETS,
-        ]);
-      } catch (err) {
-        console.log("install error", err);
-      }
-    })()
-  );
+				const cache = await caches.open(CACHE_NAME);
+				await cache.addAll([
+					new Request(OFFLINE_URL, { cache: "reload" }),
+					...ASSETS,
+				]);
+			} catch (err) {
+				console.log("install error", err);
+			}
+		})()
+	);
 
-  self.skipWaiting();
+	self.skipWaiting();
 });
 
 //update
 self.addEventListener("activate", (e) => {
-  console.log("서비스워커 동작(activation) 시작됨!");
+	console.log("서비스워커 동작(activation) 시작됨!");
 });
 
 //data fetch
 self.addEventListener("fetch", (e) => {
-  // fetch  이벤트 응답 반환. interceptor 역할할 수 있음.
-  channel.postMessage({ type: "fetch" });
-  e.respondWith(
-    (async () => {
-      try {
-        const r = await caches.match(e.request);
-        if (r) {
-          console.log("캐싱된 파일", e.request);
-          return r;
-        }
-        console.log("네트워크 요청", e.request);
+	// fetch  이벤트 응답 반환. interceptor 역할할 수 있음.
+	channel.postMessage({ type: "fetch" });
+	e.respondWith(
+		(async () => {
+			try {
+				const r = await caches.match(e.request);
+				if (r) {
+					console.log("캐싱된 파일", e.request);
+					return r;
+				}
+				console.log("네트워크 요청", e.request);
 
-        const networkResponse = await fetch(e.request);
-        return networkResponse;
-      } catch (err) {
-        console.log("Fetch failed;", err);
+				const networkResponse = await fetch(e.request);
+				return networkResponse;
+			} catch (err) {
+				console.log("Fetch failed;", err);
 
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        console.log(cachedResponse);
-        return cachedResponse;
-      }
-    })()
-  );
+				const cache = await caches.open(CACHE_NAME);
+				const cachedResponse = await cache.match(OFFLINE_URL);
+				console.log(cachedResponse);
+				return cachedResponse;
+			}
+		})()
+	);
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    (async () => {
-      try {
-        console.log("두번째 호출 ㅇㅇ");
-        const r = await caches.match(e.request);
-        if (r) {
-          return r;
-        }
+	e.respondWith(
+		(async () => {
+			try {
+				console.log("두번째 호출 ㅇㅇ");
+				const r = await caches.match(e.request);
+				console.log("fetch", r);
+				if (r) {
+					return r;
+				}
 
-        const networkResponse = await fetch(e.request);
-        return networkResponse;
-      } catch (err) {
-        console.log("Fetch failed;", err);
+				const networkResponse = await fetch(e.request);
+				return networkResponse;
+			} catch (err) {
+				console.log("Fetch failed;", err);
 
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        console.log(cachedResponse);
-        return cachedResponse;
-      }
-    })()
-  );
+				const cache = await caches.open(CACHE_NAME);
+				const cachedResponse = await cache.match(OFFLINE_URL);
+				console.log(cachedResponse);
+				return cachedResponse;
+			}
+		})()
+	);
 });
 
 //notification
 self.addEventListener("notificationclick", (event) => {
-  console.log("notificationclick");
-  let url = "https://aloveu.github.io/pwa/index.html";
-  event.notification.close(); // Android needs explicit close.
-  event.waitUntil(
-    clients.matchAll({ type: "window" }).then((windowClients) => {
-      // Check if there is already a window/tab open with the target URL
-      for (var i = 0; i < windowClients.length; i++) {
-        var client = windowClients[i];
-        // If so, just focus it.
-        if (client.url === url && "focus" in client) {
-          return client.focus();
-        }
-      }
-      // If not, then open the target URL in a new window/tab.    ㅈ
-      if (clients.openWindow) {
-        return clients.openWindow(url);
-      }
-    })
-  );
+	console.log("notificationclick");
+	let url = "https://aloveu.github.io/pwa/index.html";
+	event.notification.close(); // Android needs explicit close.
+	event.waitUntil(
+		clients.matchAll({ type: "window" }).then((windowClients) => {
+			// Check if there is already a window/tab open with the target URL
+			for (var i = 0; i < windowClients.length; i++) {
+				var client = windowClients[i];
+				// If so, just focus it.
+				if (client.url === url && "focus" in client) {
+					return client.focus();
+				}
+			}
+			// If not, then open the target URL in a new window/tab.    ㅈ
+			if (clients.openWindow) {
+				return clients.openWindow(url);
+			}
+		})
+	);
 });
 
 const unreadCount = 24;
 navigator.setAppBadge(unreadCount).catch((error) => {
-  //Do something with the error.
+	//Do something with the error.
 });
 
 let test = "";
 channel.addEventListener("message", (event) => {
-  console.log("Received", event);
-  if (event.data.type === "input") {
-    test = event.data.data.test;
-  }
-  if (event.data.type === "get") {
-    channel.postMessage({ type: "get", data: { test } });
-  }
+	console.log("Received", event);
+	if (event.data.type === "input") {
+		test = event.data.data.test;
+	}
+	if (event.data.type === "get") {
+		channel.postMessage({ type: "get", data: { test } });
+	}
 });
